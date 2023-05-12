e4a165c8fca0f6d20ed318f4833b7a11eedeedda
narspi <ivanovmichurina@gmail.com>
2023-05-12 13:19:31 +0300

    fix url

--- src/components/WeatherSummary.vue
+++ src/components/WeatherSummary.vue
@@ -13,7 +13,7 @@ const today = new Date().toLocaleString('en-EN', {weekday: 'short', year: 'numer
 <template>
   <div class="summary">
     <div
-      :style="`background-image: url('src/assets/img/weather-main/${weatherInfo?.weather[0].description}.png')`"
+      :style="`background-image: url('/weather-main/${weatherInfo?.weather[0].description}.png')`"
       class="pic-main"
     ></div>
     <div class="weather">
